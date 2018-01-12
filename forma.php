4fd22fe65b9a9fe9296c63ac0730be3539bba567
Павел Резник <35295867+Pawa13@users.noreply.github.com>
2018-01-12 17:14:53 +0200

    Delete forma.php

--- forma.php
+++
@@ -1,8 +0,0 @@
-<form action="obrabotchik_formi.php" name="myform" method="post">
-  <input name="mycolor" type="radio" value="white"> Белый
-  <input name="mycolor" type="radio" value="green" checked> Зеленый (выбран по умолчанию)
-  <input name="mycolor" type="radio" value="blue"> Синий
-  <input name="mycolor" type="radio" value="red"> Красный
-  <input name="mycolor" type="radio" value="black"> Черный
-  <INPUT TYPE="submit" value="Передать">
-</form>
